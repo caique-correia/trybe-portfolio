8c7a6a0c31d6dad0e713e0e1d0fe9f956f238a0c
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-18 19:44:32 -0300

    if/else

---
+++ 1.Fundamentos/04.JavaScript-Logica/subject4.1.js
@@ -0,0 +1,42 @@
+// VARIÁVEIS;
+let myName = 'Caíque';
+const birthCity = 'Salvador';
+const birthYear = '1998'
+
+// birthYear = birthYear + 32; 
+
+// console.log(myName, birthCity, birthYear);
+
+// TP, TD, OA; 
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+// !!#2;
+const base = 5;
+let height = 8;
+
+const area = base * height;
+const perimeter = 2 * (base + height);
+
+// console.log(area);
+// console.log(perimeter);
+
+//CONDIÇÕES IF/ELSE;
+const score = 72;
+
+if (score >= 80) {
+console.log('Parabéns, você foi aprovado(a)');
+} else if (score < 80 && score >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else if (score < 60) {
+    console.log('Você foi reprovado(a)');
+} else {
+    console.log(ERROR);
+};
+
+// OPERADORES LÓGICOS;
