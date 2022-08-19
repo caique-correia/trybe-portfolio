2bd1a0e6b8263aeda3a302b8758dac9341ccf792
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-19 16:40:56 -0300

    for break

---
+++ 1.Fundamentos/04.JavaScript-Logica/subject4.2.js
@@ -0,0 +1,12 @@
+// ARRAYS
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu [1];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+menu.push['Contato'];
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
+
+// FOR
