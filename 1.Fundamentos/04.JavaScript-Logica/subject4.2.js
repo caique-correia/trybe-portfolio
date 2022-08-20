723689d3b4457600ec36a9267351de463aca6ebb
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-19 22:26:26 -0300

    day off

--- 1.Fundamentos/04.JavaScript-Logica/subject4.2.js
+++ 1.Fundamentos/04.JavaScript-Logica/subject4.2.js
@@ -5,8 +5,37 @@ let indexOfPortfolio = menu.indexOf('Portfólio');
 
 menu.push['Contato'];
 
-console.log(menuServices);
-console.log(indexOfPortfolio);
-console.log(menu);
+// console.log(menuServices);
+// console.log(indexOfPortfolio);
+// console.log(menu);
 
 // FOR
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+}
+
+// EXERCÍCIO;
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (i = 0; i < groceryList.length; i += 1) {
+    // console.log(groceryList[i]);
+}
+
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let i of names) {
+    // console.log(i)
+}
+
+
+function dice () { 
+  return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+  d2 = dice();
+}
