988469c714758b87fe27c6fb51db9ad4e0298f41
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-29 15:09:33 -0300

    break point

--- 1.Fundamentos/05.JavaScript_DEWS/class5.2.js
+++ 1.Fundamentos/05.JavaScript_DEWS/class5.2.js
@@ -1,11 +1,11 @@
-// let furnace = document.querySelector('#elementoOndeVoceEsta');
+let furnace = document.querySelector('#elementoOndeVoceEsta');
 
-// function babyMaker(id, text) {
-//     let cast = document.createElement('section');
-//     cast.id = id;
-//     cast.innerHTML = text;
-//     return boil.appendChild(cast);
-// }
+function babyMaker(id, text) {
+	let cast = document.createElement('section');
+	cast.id = id;
+	cast.innerHTML = text;
+	return furnace.parentNode.appendChild(cast);
+}
 
 // function babyChooser() {
 //     let boil;
@@ -19,32 +19,51 @@
 //     }
 // }
 
-// console.log(babyMaker('adoptedChild', '5'));
-
-// let practiceYou = document.querySelector('#elementoOndeVoceEsta');
-
-// let thySon = document.createElement('section');
-// thySon.id = 'grandson';
-// thySon.innerHTML = '2.3';
-// practiceYou.appendChild(thySon)
-
-// let thySonsSon = document.createElement('section');
-// thySonsSon.id = 'grandgrandson';
-// thySonsSon.innerHTML = '2.1.1';
-// practiceYou.firstElementChild.appendChild(thySonsSon)
-
-// let last = practiceYou.firstElementChild.firstElementChild;
-// let last0 = last.parentNode.parentNode.nextSibling.nextSibling
-// console.log(last0)
+console.log(babyMaker('adoptedChild', '5'));
 
 let practiceYou = document.querySelector('#elementoOndeVoceEsta');
-let practice1 = practiceYou.firstElementChild
 
-function abcd (a, b, c, d) {
-    let create = document.createElement(a);
-    create.id = b;
-    create.innerHTML = c;
-    return practice1.appendChild(create);
+let thySon = document.createElement('section');
+thySon.id = 'grandson';
+thySon.innerHTML = '2.3';
+practiceYou.appendChild(thySon)
+
+let thySonsSon = document.createElement('section');
+thySonsSon.id = 'grandgrandson';
+thySonsSon.innerHTML = '2.1.1';
+practiceYou.firstElementChild.appendChild(thySonsSon)
+
+let last = practiceYou.firstElementChild.firstElementChild;
+let last0 = last.parentNode.parentNode.nextSibling.nextSibling
+// console.log(last0)
+
+// let practiceYou = document.querySelector('#elementoOndeVoceEsta');
+// let practice1 = practiceYou.firstElementChild
+
+// function abcd (a, b, c, d) {
+//     let create = document.createElement(a);
+//     create.id = b;
+//     create.innerHTML = c;
+//     return practice1.appendChild(create);
+// }
+
+// console.log(abcd('section', 'test', 'thisIsATest'))
+
+let pai = document.querySelector('#pai')
+let dad = pai.querySelectorAll('section');
+console.log(pai.childNodes);
+console.log(dad);
+
+// pai.firstElementChild.remove();
+// pai.lastElementChild.remove();
+// pai.lastElementChild.remove();
+// pai.lastElementChild.remove();
+// console.log(pai.childNodes[10])
+// console.log(document.querySelector('#pai :nth-child(5)'));
+// console.log(pai.nextElementSibling);
+
+for (let i = dad.length; i >= 0; i -= 1) {
+	if (i !== 2) {
+		pai.remove(dad[i]);
+	}
 }
-
-console.log(abcd('section', 'test', 'thisIsATest'))
