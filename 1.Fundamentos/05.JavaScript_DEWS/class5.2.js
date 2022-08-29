6c4b52d3b394d9df5decb56a0300b5ee0882771f
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-29 11:12:56 -0300

    class review

--- 1.Fundamentos/05.JavaScript_DEWS/class5.2.js
+++ 1.Fundamentos/05.JavaScript_DEWS/class5.2.js
@@ -1,15 +1,50 @@
+// let furnace = document.querySelector('#elementoOndeVoceEsta');
 
+// function babyMaker(id, text) {
+//     let cast = document.createElement('section');
+//     cast.id = id;
+//     cast.innerHTML = text;
+//     return boil.appendChild(cast);
+// }
 
-function babyMaker(x, y, place, id, text) {
-    let furnace = document.querySelector(x);
-    if (y) {
-        furnace = furnace(y)
-    }
-    let mold = document.createElement(place);
-    mold.id = id;
-    mold.innerHTML = text;
-    return furnace.append(mold);
+// function babyChooser() {
+//     let boil;
+//     switch (true) {
+//         case type === 'parentNode':
+//             boil = furnace.parentNode;
+//             break;
+//         case type === 'firstElementChild':
+//             boil = furnace.firstElementChild
+
+//     }
+// }
+
+// console.log(babyMaker('adoptedChild', '5'));
+
+// let practiceYou = document.querySelector('#elementoOndeVoceEsta');
+
+// let thySon = document.createElement('section');
+// thySon.id = 'grandson';
+// thySon.innerHTML = '2.3';
+// practiceYou.appendChild(thySon)
+
+// let thySonsSon = document.createElement('section');
+// thySonsSon.id = 'grandgrandson';
+// thySonsSon.innerHTML = '2.1.1';
+// practiceYou.firstElementChild.appendChild(thySonsSon)
+
+// let last = practiceYou.firstElementChild.firstElementChild;
+// let last0 = last.parentNode.parentNode.nextSibling.nextSibling
+// console.log(last0)
+
+let practiceYou = document.querySelector('#elementoOndeVoceEsta');
+let practice1 = practiceYou.firstElementChild
+
+function abcd (a, b, c, d) {
+    let create = document.createElement(a);
+    create.id = b;
+    create.innerHTML = c;
+    return practice1.appendChild(create);
 }
 
-console.log(babyMaker('#elementoOndeVoceEsta', '.parentNode', 'section', 'adoptedChild', '5'));
-console.log(babyMaker)
+console.log(abcd('section', 'test', 'thisIsATest'))
