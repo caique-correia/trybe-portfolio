60d997194d9d08a8e63a50b98ce01f8319e4ad72
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-30 19:03:35 -0300

    css baseline

---
+++ 1.Fundamentos/05.JavaScript_DEWS/subject5.4.js
@@ -0,0 +1,10 @@
+const mainArticle = document.getElementById('mainArticle');
+const listsSection = document.getElementById('listsSection');
+const mainOl = document.getElementById('mainOl');
+const mainUl = document.getElementById('mainUl');
+const textInputAside = document.getElementById('textInputAside');
+const buttonInputAside = document.getElementById('buttonInputAside');
+
+// console.log(mainArticle);
+// console.log(mainArticle.childNodes);
+
