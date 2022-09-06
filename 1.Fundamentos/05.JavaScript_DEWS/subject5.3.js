358bdde8f620e8ac8970826685f55c538d8d9cb3
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-09-06 12:13:03 -0300

    up to q3

--- 1.Fundamentos/05.JavaScript_DEWS/subject5.3.js
+++ 1.Fundamentos/05.JavaScript_DEWS/subject5.3.js
@@ -19,7 +19,7 @@ createDaysOfTheWeek();
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function decemberDays(x) {
-  const ulDays = document.getElementById('days');
+  const getDaysUl = document.getElementById('days');
 
   for (let i = 0; i < x.length; i += 1) {
     const newLi = document.createElement('li');
@@ -31,7 +31,7 @@ function decemberDays(x) {
       newLi.classList.add('friday');
     }
     newLi.innerText = x[i];
-    ulDays.appendChild(newLi);
+    getDaysUl.appendChild(newLi);
   }
 }
 
@@ -39,15 +39,51 @@ decemberDays(decemberDaysList);
 
 // Button Holiday;
 
-function btnHoliday() {
-  const btnCont = document.getElementsByClassName('buttons-container')[0];
-  const newBtn = document.createElement('button');
-  newBtn.id = 'btn-holiday';
-  newBtn.innerHTML = 'Feriados';
-  btnCont.appendChild(newBtn);
+const getButtonsContainer = document.getElementsByClassName('buttons-container');
+
+function addHolidayButton() {
+  const newButton = document.createElement('button');
+  newButton.id = 'btn-holiday';
+  newButton.innerHTML = 'Feriados';
+  getButtonsContainer[0].appendChild(newButton);
 }
 
-btnHoliday();
+addHolidayButton();
 
 // Background Holiday;
 
+const getButtonHolidayId = document.getElementById('btn-holiday');
+const getHolidayClass = document.getElementsByClassName('holiday');
+const getDayClass = document.getElementsByClassName('day');
+const naturalBgColor = 'rgb(238,238,238)';
+const naturalLtColor = 'rgb(119,119,119)';
+const holidayBgColor = 'green';
+const holidayLtColor = 'white';
+
+function applyNaturalColor() {
+  for (let i = 0; i < getDayClass.length; i += 1) {
+    getDayClass[i].style.backgroundColor = naturalBgColor;
+    getDayClass[i].style.color = naturalLtColor;
+  }
+}
+
+applyNaturalColor()
+
+function onClickHolidayColor() {
+  const holidayClassColor = getHolidayClass[0].style.backgroundColor;
+  if (holidayClassColor === naturalBgColor || holidayClassColor !== holidayBgColor) {
+    for (let i = 0; i < getHolidayClass.length; i += 1) {
+      getHolidayClass[i].style.backgroundColor = holidayBgColor;
+      getHolidayClass[i].style.color = holidayLtColor;
+    }
+  } else {
+    for (let i = 0; i < getHolidayClass.length; i += 1) {
+      getHolidayClass[i].style.backgroundColor = naturalBgColor;
+      getHolidayClass[i].style.color = naturalLtColor;
+    }
+  }
+}
+
+getButtonHolidayId.addEventListener('click', onClickHolidayColor);
+
+// Add Friday Button;
