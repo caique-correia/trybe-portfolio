0f5d3ad77703a4a8fd9fc092e3babf04767b02a6
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-09-18 19:14:04 -0300

    s7.2 part1

--- 1.Fundamentos/07.JavaScript_ES6-Tests/subject_7.2.js
+++ 1.Fundamentos/07.JavaScript_ES6-Tests/subject_7.2.js
@@ -2,33 +2,48 @@
 const valueInput1 = document.getElementById('value1');
 const valueInput2 = document.getElementById('value2');
 const resultP = document.getElementById('result')
+const button = document.getElementById('button');
 
-const validateValues = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    throw new Error('Please, stop!')
+const verifyIsEmpty = () => {
+  if (valueInput1.value === '' || valueInput2.value === '') {
+    throw new Error('Todos os campos devem ser preenchidos!');
   }
-}
+};
 
-const elaborateValues = (value1, value2) => {
+const verifyIsNaN = (value1, value2) => {
+  if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    throw new Error('Válidos apenas caracteres númericos!');
+  }
+};
+
+const printResult = (value1, value2) => {
   const result = value1 + value2;
-  console.log(typeof result)
   resultP.innerHTML = `Resultado: ${result}`;
+};
+
+const printError = error => resultP.innerHTML = error;
+
+const resetValues = () => {
   valueInput1.value = '';
   valueInput2.value = '';
-}
+};
 
-function sum() {
+const sum = () => {
   const value1 = Number(valueInput1.value);
   const value2 = Number(valueInput2.value);
 
   try {
-    validateValues(value1, value2);
-    elaborateValues(value1, value2);
-  } catch (e) {
-    console.log('Yes!')
+    verifyIsEmpty();
+    verifyIsNaN(value1, value2);
+    printResult(value1, value2);
+  } catch (error) {
+    printError(error);
+    console.warn(error);
+  } finally {
+    resetValues();
   }
-}
+};
+
 window.onload = () => {
-  const button = document.getElementById('button');
   button.addEventListener('click', sum);
 }
