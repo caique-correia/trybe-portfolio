b3aee5f45716d0cfc77948b5cb8d3861120975bc
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-09-22 20:45:17 -0300

    sub_8.1 sorteador

--- 1.Fundamentos/08.HOF_JavaScript_ES6/subject_8.1.js
+++ 1.Fundamentos/08.HOF_JavaScript_ES6/subject_8.1.js
@@ -1,3 +1,4 @@
+// NOVA PESSOA CONTRATADA;
 const newEmployees = (callback) => {
   const employees = {
     id1: callback('Pedro Guerra'),
@@ -9,5 +10,13 @@ const newEmployees = (callback) => {
 
 const generateEmail = (name) => `${name.toLowerCase().replace(' ', '_')}@trybe.com`;
 const newEmployee = (name) => ({ Nome: name, Email: generateEmail(name) });
-console.log(newEmployees(newEmployee));
 
+// console.log(newEmployees(newEmployee));
+
+// SORTEADOR DE LOTERIA;
+const randomNumberGenerator = (maxResult) => Math.ceil(Math.random() * maxResult);
+const compareLuckyNumber = (luckyNumber, RNG) => luckyNumber === RNG(5) ? 'Parabéns, você ganhou!' : 'Tente novamente...';
+
+// console.log(compareLuckyNumber(5, randomNumberGenerator));
+
+// CORRETOR AUTOMÁTICO DE EXAME;
