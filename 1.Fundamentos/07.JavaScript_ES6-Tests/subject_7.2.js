94c3d832ab48daa2604efeb1ac062518df02c396
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-09-25 18:23:01 -0300

    sub_7.2 1/3

--- 1.Fundamentos/07.JavaScript_ES6-Tests/subject_7.2.js
+++ 1.Fundamentos/07.JavaScript_ES6-Tests/subject_7.2.js
@@ -1,49 +1,106 @@
-// First;
-const valueInput1 = document.getElementById('value1');
-const valueInput2 = document.getElementById('value2');
-const resultP = document.getElementById('result')
-const button = document.getElementById('button');
+// // PART I;
+// const valueInput1 = document.getElementById('value1');
+// const valueInput2 = document.getElementById('value2');
+// const resultP = document.getElementById('result')
+// const button = document.getElementById('button');
 
-const verifyIsEmpty = () => {
-  if (valueInput1.value === '' || valueInput2.value === '') {
-    throw new Error('Todos os campos devem ser preenchidos!');
-  }
+// const verifyIsEmpty = () => {
+//   if (valueInput1.value === '' || valueInput2.value === '') {
+//     throw new Error('Todos os campos devem ser preenchidos!');
+//   }
+// };
+
+// const verifyIsNaN = (value1, value2) => {
+//   if (Number.isNaN(value1) || Number.isNaN(value2)) {
+//     throw new Error('Válidos apenas caracteres númericos!');
+//   }
+// };
+
+// const printResult = (value1, value2) => {
+//   const result = value1 + value2;
+//   resultP.innerHTML = `Resultado: ${result}`;
+// };
+
+// const printError = error => resultP.innerHTML = error;
+
+// const resetValues = () => {
+//   valueInput1.value = '';
+//   valueInput2.value = '';
+// };
+
+// const sum = () => {
+//   const value1 = Number(valueInput1.value);
+//   const value2 = Number(valueInput2.value);
+
+//   try {
+//     verifyIsEmpty();
+//     verifyIsNaN(value1, value2);
+//     printResult(value1, value2);
+//   } catch (error) {
+//     printError(error);
+//     console.warn(error);
+//   } finally {
+//     resetValues();
+//   }
+// };
+
+// window.onload = () => {
+//   button.addEventListener('click', sum);
+// }
+
+// PART II;
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
 };
 
-const verifyIsNaN = (value1, value2) => {
-  if (Number.isNaN(value1) || Number.isNaN(value2)) {
-    throw new Error('Válidos apenas caracteres númericos!');
-  }
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  return `Olá, ${order.order.delivery.deliveryPerson}! Entrega para: 
+  ${order.name}, Telefone: ${order.phoneNumber}, 
+  R. ${order.address.street}, Nº ${order.address.number}, AP: ${order.address.apartment}`;
 };
 
-const printResult = (value1, value2) => {
-  const result = value1 + value2;
-  resultP.innerHTML = `Resultado: ${result}`;
+// console.log(customerInfo(order));
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const newCostumer = order.name = 'Luiz Silva';
+  const newPrice = order.payment.total = '50';
+  const flavors = Object.keys(order.order.pizza);
+  return `Olá ${newCostumer}, o total do seu pedido de 
+  ${flavors[0]}, ${flavors[1]} e ${order.order.drinks.coke.type} é R$${newPrice},00.`
 };
 
-const printError = error => resultP.innerHTML = error;
-
-const resetValues = () => {
-  valueInput1.value = '';
-  valueInput2.value = '';
-};
-
-const sum = () => {
-  const value1 = Number(valueInput1.value);
-  const value2 = Number(valueInput2.value);
-
-  try {
-    verifyIsEmpty();
-    verifyIsNaN(value1, value2);
-    printResult(value1, value2);
-  } catch (error) {
-    printError(error);
-    console.warn(error);
-  } finally {
-    resetValues();
-  }
-};
-
-window.onload = () => {
-  button.addEventListener('click', sum);
-}
+console.log(orderModifier(order));
