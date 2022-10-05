852c5ad93015ee86d97a477c2fed4b8706ad868a
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-10-05 12:16:07 -0300

    sub_7.3 Q2

--- 1.Fundamentos/07.JavaScript_ES6-Tests/7.3_Section/subject_7.3.test.js
+++ 1.Fundamentos/07.JavaScript_ES6-Tests/7.3_Section/subject_7.3.test.js
@@ -1,5 +1,5 @@
 const {
-  sum,
+  sum, myRemove,
 } = require('./subject_7.3');
 
 describe('Question 1', () => {
@@ -16,3 +16,27 @@ describe('Question 1', () => {
     expect(() => sum(4, '5')).toThrow('parameters must be numbers');
   });
 });
+
+describe('Question 2', () => {
+  it('Return expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  it('Not return unexpected array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  it('Return expected array', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+// describe('Question 3', () => {
+//   it('', () => {
+//     expect(myRemove()).toBe();
+//   });
+//   it('', () => {
+//     expect(myRemove()).toBe();
+//   });
+//   it('', () => {
+//     expect(myRemove()).toBe();
+//   });
+// });
