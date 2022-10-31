44f4a66b600ec90305608d7944eed2803c103dc9
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-10-31 19:43:34 -0300

    cls_4.1 baseline

---
+++ 2.Front-End/04.Lifecycle_R-Router/4.1_Section/4.1_class_react/src/components/DadJoke.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class DadJoke extends React.Component {
+  constructor() {
+    super();
+
+    this.saveJoke = this.saveJoke.bind(this);
+
+    this.state = {
+      jokeObj: undefined,
+      loading: true,
+      storedJokes: [],
+    }
+  }
+
+  async fetchJoke() {
+    const requestHeaders = { headers: { Accept: 'application/json' } }
+    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+    const requestObject = await requestReturn.json();
+    this.setState({
+      jokeObj: requestObject,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+
+  saveJoke() {}
+
+  render() {
+    const { storedJokes } = this.state;
+    // const loadingElement = <span>Loading...</span>;
+
+    return (
+      <div>
+        <span>
+          {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default DadJoke;
