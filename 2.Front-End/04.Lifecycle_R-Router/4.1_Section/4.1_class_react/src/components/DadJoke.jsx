9a2bee87a201b559c644508ac897f4a8089534dd
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-01 13:52:31 -0300

    cls_4.1 DadJoke double mount

--- 2.Front-End/04.Lifecycle_R-Router/4.1_Section/4.1_class_react/src/components/DadJoke.jsx
+++ 2.Front-End/04.Lifecycle_R-Router/4.1_Section/4.1_class_react/src/components/DadJoke.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RenderJoke from './RenderJoke';
 
 class DadJoke extends React.Component {
   constructor() {
@@ -14,29 +15,39 @@ class DadJoke extends React.Component {
   }
 
   async fetchJoke() {
-    const requestHeaders = { headers: { Accept: 'application/json' } }
-    const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-    const requestObject = await requestReturn.json();
-    this.setState({
-      jokeObj: requestObject,
+    this.setState({ loading: true }, async () => {
+      const requestHeaders = { headers: { Accept: 'application/json' } }
+      const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+      const requestObject = await requestReturn.json();
+      this.setState({
+        jokeObj: requestObject,
+        loading: false,
+      })
     })
   }
 
-  componentDidMount() {
+  saveJoke() {
+    this.setState(({ jokeObj, storedJokes }) => ({ storedJokes: [...storedJokes, jokeObj] }))
     this.fetchJoke();
   }
 
-  saveJoke() {}
+  componentDidMount() {
+    console.log('mount')
+    this.fetchJoke();
+  }
 
   render() {
-    const { storedJokes } = this.state;
-    // const loadingElement = <span>Loading...</span>;
+    const { jokeObj, loading, storedJokes } = this.state;
+    const loadingElement = <span>Loading...</span>;
 
     return (
       <div>
         <span>
           {storedJokes.map(({ id, joke }) => (<p key={id}>{joke}</p>))}
         </span>
+        {loading
+          ? loadingElement
+          : <RenderJoke jokeObj={jokeObj} saveJoke={this.saveJoke} />}
       </div>
     );
   }
