6135f02ea6965973401a267236922194ee3819e7
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-18 19:44:42 -0300

    cls_6.2

--- 2.Front-End/06.RTL/6.2_Section/6.2_class_react/src/tests/App.test.js
+++ 2.Front-End/06.RTL/6.2_Section/6.2_class_react/src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from '../components/App';
 
 afterEach(() => jest.clearAllMocks());
@@ -61,3 +62,46 @@ it('fetches a joke, third form', async () => {
   expect(global.fetch).toHaveBeenCalledTimes(1);
   expect(global.fetch).toHaveBeenCalledWith('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
 });
+
+it('fetches when button is clicked', async () => {
+  const firstJoke = {
+    id: 'MyNameIsNot7',
+    joke: "But is it t'see or t'eat",
+    status: 200,
+  };
+
+  const secondJoke = {
+    id: 'MyNameIsIndeed7',
+    joke: "Do you like pineapplere juice?",
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(firstJoke),
+  });
+
+  render (<App />);
+  const newJokeButton = screen.getByRole('button', { name: 'New Joke' });
+  const renderFirstJoke = await screen.findByText(firstJoke.joke);
+  const seachSecondJoke = screen.queryByText(secondJoke.joke);
+  // const newJokeButton = await screen.findByRole('button', { name: 'New Joke' });
+
+  expect(renderFirstJoke).toBeInTheDocument();
+  expect(seachSecondJoke).not.toBeInTheDocument();
+  expect(newJokeButton).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(secondJoke),
+  });
+
+  userEvent.click(newJokeButton);
+  
+  const renderSecondJoke = await screen.findByText(secondJoke.joke);
+  const seachFirstJoke = screen.queryByText(firstJoke.joke);
+
+  expect(renderSecondJoke).toBeInTheDocument();
+  expect(seachFirstJoke).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(2);
+})
