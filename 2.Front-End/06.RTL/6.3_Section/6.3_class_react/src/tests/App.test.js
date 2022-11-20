a50d5d29e32541f250e686ba807351b2a8b8bcda
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-20 19:13:21 -0300

    sub_6.3 to e-digimon-finders

--- 2.Front-End/06.RTL/6.3_Section/6.3_class_react/src/tests/App.test.js
+++ 2.Front-End/06.RTL/6.3_Section/6.3_class_react/src/tests/App.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../components/renderWithRouter';
 import App from '../components/App';
 
-it('deve renderizar o componente App', () => {
-  renderWithRouter(<App />);
+describe('Teste de aplicação completo', () => {
+  it('Testa renderização /App', () => {
+    renderWithRouter(<App />);
 
-  const homeTitle = screen.getByRole('heading', {
-    name: 'Você está na página Início',
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
   });
-  expect(homeTitle).toBeInTheDocument();
-});
 
-it('deve renderizar o componente /Sobre', () => {
-  const { history } = renderWithRouter(<App />);
-  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
-  expect(aboutLink).toBeInTheDocument();
-  userEvent.click(aboutLink);
+  it('Testa renderização About', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
 
-  const { pathname } = history.location;
-  expect(pathname).toBe('/about');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
 
-  const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' });
-  expect(aboutTitle).toBeInTheDocument();
+    const aboutTitle = screen.getByRole('heading', { name: 'Você está na página Sobre' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Testa renderização NoMatch', () => {
+    const { history } = renderWithRouter(<App />)
+
+    act(() => {
+      history.push('/non/existent/page')
+    })
+
+    const notFoundTitle = screen.getByRole('heading', { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  })
 });
