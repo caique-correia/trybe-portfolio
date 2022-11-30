763c6589d9701b46090f594430dc9d6f74aa19b0
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-11-30 18:14:49 -0300

    connect mapStateToProps

--- 2.Front-End/07.State_Management_Redux/7.2_Section/7.2_class_react/src/components/Counter.jsx
+++ 2.Front-End/07.State_Management_Redux/7.2_Section/7.2_class_react/src/components/Counter.jsx
@@ -1,16 +1,22 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Counter extends Component {
   render() {
+    const { countState } = this.props;
     return (
-    <>
-      <h1>Counter Activite</h1>
-      <h2>0</h2>
-      <button id='add1' type='button'>+1</button>
-      <button id='add5' type='button'>+5</button>
-    </>
+      <>
+        <h1>Counter Activite</h1>
+        <h2>{countState}</h2>
+        <button id='add1' type='button'>+1</button>
+        <button id='add5' type='button'>+5</button>
+      </>
     );
   }
 }
 
-export default Counter;
+const mapStateToProps = (state) => ({
+  countState: state.count,
+})
+
+export default connect(mapStateToProps)(Counter);
