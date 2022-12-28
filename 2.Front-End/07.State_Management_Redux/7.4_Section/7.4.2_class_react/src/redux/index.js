ab2ee991e0038dbdbf083d5bf40e7dc67bd64d94
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-12-28 18:14:24 -0300

    cls_7.4.2

--- 2.Front-End/07.State_Management_Redux/7.4_Section/7.4.2_class_react/src/redux/index.js
+++ 2.Front-End/07.State_Management_Redux/7.4_Section/7.4.2_class_react/src/redux/index.js
@@ -1,5 +1,7 @@
-import { legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk'
 import reducer from './reducers/';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
