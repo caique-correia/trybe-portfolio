33e8a6c2eab86384ed713ca4a3d738439c774eb6
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-01-17 16:50:26 -0300

    transfer form from /App to /Form

--- 2.Front-End/09.Context_API-React_Hooks/9.1_Section/9.1.1_class_react/src/App.js
+++ 2.Front-End/09.Context_API-React_Hooks/9.1_Section/9.1.1_class_react/src/App.js
@@ -1,83 +1,10 @@
-import { useState } from 'react';
+import Form from './components/Form';
 import './style/App.css';
 
-function App() {
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-  const [city, setCity] = useState();
-  const [module, setModule] = useState('Fundamentos');
-
-  return (
+function App() {return (
     <div className="App">
       <header className="App-header">
-        <input
-          type="text"
-          value={name}
-          placeholder="Full Name"
-          onChange={({ target }) => setName(target.value)}
-        />
-
-        <input
-          type="number"
-          value={age}
-          placeholder="Age"
-          onChange={({ target }) => setAge(target.value)}
-        />
-
-        <input
-          type="text"
-          value={city}
-          placeholder="City"
-          onChange={({ target }) => setCity(target.value)}
-        />
-
-        <label htmlFor="Fundamentos">
-          <input
-            type="radio"
-            id="Fundamentos"
-            name="module"
-            value="Fundamentos"
-            checked={module === 'Fundamentos'}
-            onClick={() => setModule('Fundamentos')}
-          />
-          Fundamentos
-        </label>
-
-        <label htmlFor="Front-end">
-          <input
-            type="radio"
-            id="Front-end"
-            name="module"
-            value="Front-end"
-            checked={module === 'Front-end'}
-            onClick={() => setModule('Front-end')}
-          />
-          Front-end
-        </label>
-
-        <label htmlFor="Back-end">
-          <input
-            type="radio"
-            id="Back-end"
-            name="module"
-            value="Back-end"
-            checked={module === 'Back-end'}
-            onClick={() => setModule('Back-end')}
-          />
-          Back-end
-        </label>
-
-        <label htmlFor="Ciência da Computação">
-          <input
-            type="radio"
-            id="Ciência da Computação"
-            name="module"
-            value="Ciência da Computação"
-            checked={module === 'Ciência da Computação'}
-            onClick={() => setModule('Ciência da Computação')}
-          />
-          Ciência da Computação
-        </label>
+        <Form />
 
       </header>
     </div>
---
+++ 2.Front-End/09.Context_API-React_Hooks/9.1_Section/9.1.1_class_react/src/components/Form.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+
+function Form() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [city, setCity] = useState('');
+  const [module, setModule] = useState('Fundamentos');
+
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        placeholder="Full Name"
+        onChange={({ target }) => setName(target.value)}
+      />
+
+      <input
+        type="number"
+        value={age}
+        placeholder="Age"
+        onChange={({ target }) => setAge(target.value)}
+      />
+
+      <input
+        type="text"
+        value={city}
+        placeholder="City"
+        onChange={({ target }) => setCity(target.value)}
+      />
+
+      <label htmlFor="Fundamentos">
+        <input
+          type="radio"
+          id="Fundamentos"
+          name="module"
+          value="Fundamentos"
+          checked={module === 'Fundamentos'}
+          onClick={() => setModule('Fundamentos')}
+        />
+        Fundamentos
+      </label>
+
+      <label htmlFor="Front-end">
+        <input
+          type="radio"
+          id="Front-end"
+          name="module"
+          value="Front-end"
+          checked={module === 'Front-end'}
+          onClick={() => setModule('Front-end')}
+        />
+        Front-end
+      </label>
+
+      <label htmlFor="Back-end">
+        <input
+          type="radio"
+          id="Back-end"
+          name="module"
+          value="Back-end"
+          checked={module === 'Back-end'}
+          onClick={() => setModule('Back-end')}
+        />
+        Back-end
+      </label>
+
+      <label htmlFor="Ciência da Computação">
+        <input
+          type="radio"
+          id="Ciência da Computação"
+          name="module"
+          value="Ciência da Computação"
+          checked={module === 'Ciência da Computação'}
+          onClick={() => setModule('Ciência da Computação')}
+        />
+        Ciência da Computação
+      </label>
+    </>
+  )
+}
+
+export default Form;
