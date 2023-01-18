fc2240b523024c219aeccbd5678ebbbb7c81ee19
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-01-18 17:34:20 -0300

    sub_9.1.1;

---
+++ 2.Front-End/09.Context_API-React_Hooks/9.1_Section/subject_9.1.1_react/src/App.js
@@ -0,0 +1,14 @@
+import MainPage from './components/MainPage';
+import './style/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MainPage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
