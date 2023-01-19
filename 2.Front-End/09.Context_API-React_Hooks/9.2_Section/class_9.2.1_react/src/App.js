0f89e81be47feb8f2f6c0cf1708c613005addbb9
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-01-19 11:47:17 -0300

    cls_9.2.1;

--- 2.Front-End/09.Context_API-React_Hooks/9.2_Section/class_9.2.1_react/src/App.js
+++ 2.Front-End/09.Context_API-React_Hooks/9.2_Section/class_9.2.1_react/src/App.js
@@ -1,25 +1,16 @@
-import { useState } from 'react';
-import ThemeContext from './context/ThemeContext';
+import ThemeProvider from './context/ThemeProvider';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Image from './components/Image';
 import './style/style.css'
 
 function App() {
-  const [themeColor, setThemeColor] = useState('dark')
-  
-  function toggleTheme() {
-    setThemeColor(themeColor === 'dark' ? 'light' : 'dark');
-  }
-
   return (
-   <ThemeContext.Provider value={{color: themeColor, toggleTheme}}>
-    <div className={`app ${themeColor}`}>
-      <Header />
-      <Image />
-      <Footer />
-    </div>
-   </ThemeContext.Provider>
+    <ThemeProvider>
+        <Header />
+        <Image />
+        <Footer />
+    </ThemeProvider>
   );
 }
 
