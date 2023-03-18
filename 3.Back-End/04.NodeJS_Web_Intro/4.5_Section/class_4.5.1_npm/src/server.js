5bb109f4e3e09163091e7f5030e4c7bd47834e20
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-03-18 19:11:30 -0300

    feat: cls_4.5 completion;

--- 3.Back-End/04.NodeJS_Web_Intro/4.5_Section/class_4.5.1_npm/src/server.js
+++ 3.Back-End/04.NodeJS_Web_Intro/4.5_Section/class_4.5.1_npm/src/server.js
@@ -4,4 +4,5 @@ const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`TrybeCash API executed on port ${PORT}`);
+  console.log(`Valor da variável de ambiente $USER: ${process.env.USER}`);
 });
