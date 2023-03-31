d1e9bb904d1abb5f4294d5a290377fdb67d93c98
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-03-31 17:46:05 -0300

    feat: cls_6.1.1/Operations;

--- 3.Back-End/06.NodeJS_ORM_Auth/6.1_Section/class_6.1.1_sequelize/src/seeders/20230330215606-users.js
+++ 3.Back-End/06.NodeJS_ORM_Auth/6.1_Section/class_6.1.1_sequelize/src/seeders/20230330215606-users.js
@@ -6,7 +6,6 @@ module.exports = {
       {
         fullName: 'Leonardo',
         email: 'leo@test.com',
-        // usamos a função CURRENT_TIMESTAMP do SQL para salvar a data e hora atual nos campos `createdAt` e `updatedAt`
         createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
         updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
