ecc0bf1b816b4a5f1921981276aa8784e0c36f86
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-05-08 18:55:45 -0300

    feat: cls_9.4/SRP;

---
+++ 3.Back-End/09.POO_Solid/9.4_Section/class_9.4.1_poo/src/toFix/ReadingTracker.ts
@@ -0,0 +1,21 @@
+class ReadingTracker {
+  private readingGoal: number;
+  private booksRead: number;
+  constructor(readingGoal: number) {
+    this.readingGoal = readingGoal;
+    this.booksRead = 0;
+  }
+
+  trackReadings(readsCount: number): void {
+    this.booksRead += readsCount;
+    if (this.booksRead >= this.readingGoal) {
+      console.log('Congratulations! You\'ve reached your reading goal!');
+      return;
+    }
+    console.log('There are still some books to go!');
+  }
+}
+
+const readTracker = new ReadingTracker(20);
+readTracker.trackReadings(12);
+readTracker.trackReadings(9);
