6d5054e5612e4c5c3439174072c1d21dbd9a0df0
CGC-98 <caiquecorreia.gama@hotmail.com>
2023-05-08 19:56:12 -0300

    feat: cls_9.4/OCP_Example;

--- 3.Back-End/09.POO_Solid/9.4_Section/class_9.4.1_poo/src/index.ts
+++ 3.Back-End/09.POO_Solid/9.4_Section/class_9.4.1_poo/src/index.ts
@@ -1,4 +1,7 @@
-// ./src/index.ts
+type School = {
+  name: string;
+  approvalGrade: number;
+}
 
 type Discipline = {
   name: string;
@@ -9,7 +12,7 @@ type Discipline = {
 type Student = {
   name: string;
   disciplines: Discipline[];
-  school?: string;
+  school: School;
 };
 
 /* Apoio para a função `getGradeLetter` */
@@ -41,8 +44,8 @@ const percentageGradesIntoLetters = (student: Student): Student => ({
 });
 
 /* "Determinar" */
-const approvedStudents = ({ disciplines }: Student): boolean =>
-  disciplines.every(({ grade }) => grade > 0.7);
+const approvedStudents = ({ disciplines, school }: Student): boolean =>
+  disciplines.every(({ grade }) => grade > school.approvalGrade);
 
 /* "Atualizar" */
 const updateApprovalData = (student: Student): void => {
@@ -58,6 +61,27 @@ function setApproved(students: Student[]): void {
     .filter(approvedStudents)
     .map(updateApprovalData);
 }
+
+const studentsExample = [
+  {
+    name: 'Lee',
+    school: { name: 'Standard', approvalGrade: 0.7 },
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+  {
+    name: 'Albus',
+    school: { name: 'Hogwarts', approvalGrade: 0.8 },
+    disciplines: [
+      { name: 'divination', grade: 0.8 },
+      { name: 'potions', grade: 0.9 },
+    ],
+  },
+];
+
+setApproved(studentsExample);
 
 export {
   percentageGradesIntoLetters,
